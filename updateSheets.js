18a194b6fd4d4d47118a3c3c74935f658a54ebc9
Carl-Johan Kjellander <carljohan@kjellander.com>
2019-01-14 20:48:44 +0100

    indent formdata

--- updateSheets.js
+++ updateSheets.js
@@ -91,65 +91,65 @@ function parseData(data) {
 }
 
 function getData(range_step, range_max, mv, temp, hpa, hum, hgt_sgt) {
- var formData =
-{ "b_id.v":"-1"
-, "bc.v":"0.274"
-// G1 = 0, G7 = 4
-, "d_f.v":"4"
-, "bt_wgt.v":"130"
-, "bt_wgt.u":"23"
-, "cal.v":"6.5"
-, "cal.u":"9"
-, "m_vel.v":mv
-, "m_vel.u":"17"
-, "ch_dst.v":"0"
-, "ch_dst.u":"12"
-, "hgt_sgt.v":hgt_sgt
-, "hgt_sgt.u":"11"
-, "ofs_sgt.v":"0.0"
-, "ofs_sgt.u":"11"
-, "hgt_zer.v":"0.0"
-, "hgt_zer.u":"11"
-, "ofs_zer.v":"0.0"
-, "ofs_zer.u":"11"
-, "azm.v":"0.0"
-, "azm.u":"2"
-, "ele.v":"0.0"
-, "ele.u":"2"
-, "los.v":"0.0"
-, "cnt.v":"0.0"
-, "spd_wnd.v":"10"
-, "spd_wnd.u":"17"
-, "ang_wnd.v":"90.0"
-, "spd_tgt.v":"5"
-, "spd_tgt.u":"17"
-, "ang_tgt.v":"90.0"
-, "siz_tgt.v":"30"
-, "siz_tgt.u":"11"
-, "rng_min.v":"0"
-, "rng_max.v":range_max
-, "rng_inc.v":range_step
-, "rng_zer.v":"100"
-, "tmp.v":temp
-, "tmp.u":"18"
-, "prs.v":hpa
-, "prs.u":"21"
-, "hum.v":hum
-, "alt.v":"0.0"
-, "alt.u":"12"
-, "rad_vz.v":"12.5"
-, "rad_vz.u":"11"
-, "col_eng.v":"1"
-, "col1_un.v":"1.00"
-, "col1_un.u":"11"
-, "col2_un.v":"1.00"
-, "col2_un.u":"2"
-, "cor_ele.v": "on"
-, "rng_un.v":  "on"
-, "def_cnt.v": "on"
-//, "mrk_trs.v": "on"
-, "inc_ds.v": "on"
-};
+  var formData =
+      { "b_id.v":"-1"
+        , "bc.v":"0.274"
+        // G1 = 0, G7 = 4
+        , "d_f.v":"4"
+        , "bt_wgt.v":"130"
+        , "bt_wgt.u":"23"
+        , "cal.v":"6.5"
+        , "cal.u":"9"
+        , "m_vel.v":mv
+        , "m_vel.u":"17"
+        , "ch_dst.v":"0"
+        , "ch_dst.u":"12"
+        , "hgt_sgt.v":hgt_sgt
+        , "hgt_sgt.u":"11"
+        , "ofs_sgt.v":"0.0"
+        , "ofs_sgt.u":"11"
+        , "hgt_zer.v":"0.0"
+        , "hgt_zer.u":"11"
+        , "ofs_zer.v":"0.0"
+        , "ofs_zer.u":"11"
+        , "azm.v":"0.0"
+        , "azm.u":"2"
+        , "ele.v":"0.0"
+        , "ele.u":"2"
+        , "los.v":"0.0"
+        , "cnt.v":"0.0"
+        , "spd_wnd.v":"10"
+        , "spd_wnd.u":"17"
+        , "ang_wnd.v":"90.0"
+        , "spd_tgt.v":"5"
+        , "spd_tgt.u":"17"
+        , "ang_tgt.v":"90.0"
+        , "siz_tgt.v":"30"
+        , "siz_tgt.u":"11"
+        , "rng_min.v":"0"
+        , "rng_max.v":range_max
+        , "rng_inc.v":range_step
+        , "rng_zer.v":"100"
+        , "tmp.v":temp
+        , "tmp.u":"18"
+        , "prs.v":hpa
+        , "prs.u":"21"
+        , "hum.v":hum
+        , "alt.v":"0.0"
+        , "alt.u":"12"
+        , "rad_vz.v":"12.5"
+        , "rad_vz.u":"11"
+        , "col_eng.v":"1"
+        , "col1_un.v":"1.00"
+        , "col1_un.u":"11"
+        , "col2_un.v":"1.00"
+        , "col2_un.u":"2"
+        , "cor_ele.v": "on"
+        , "rng_un.v":  "on"
+        , "def_cnt.v": "on"
+        //, "mrk_trs.v": "on"
+        , "inc_ds.v": "on"
+      };
 
   var options = {
     'method': 'post',
